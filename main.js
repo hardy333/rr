194bb26a781f7e02a10229a1946f8c3c33ab77b6
Gela Samsonidze <samosnidze7@yahoo.com>
2020-09-07 17:00:53 +0400

    modified css

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log("In main.js file");
+
+
+function sampleFunction(name){
+
+const lastName = "Samsonidze";
+	return  name + lastName;
+}
+
+
+const person = sampleFunction("gela");
+
+console.log(person);
